8e5004f1182f4eed001ac64252528743e37d10a3
Ahmad Malhadi <hexdemsion@gmail.com>
2021-10-29 18:30:50 +0800

    changing primary logo for icon worker

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-self.addEventListener('install', (e) => {
- e.waitUntil(
-   caches.open('calc_schlumberger').then((cache) => {
-     return cache.addAll([
-       "./",
-       "./index.html",
-       "./img/logo-white.png",
-
-       "./img/demo/favicon.ico",
-       "./img/demo/apple-touch-icon.png",
-       "./img/demo/apple-touch-icon-57x57.png",
-       "./img/demo/apple-touch-icon-72x72.png",
-       "./img/demo/apple-touch-icon-76x76.png",       
-       "./css/bootstrap.min.css",
-       "./js/object.js",
-       "./js/formula.js",
-       "./js/form.js",
-       ].map(url => new Request(url, {credentials: 'same-origin'})))
-   })
-   )
-})
 
 
 self.addEventListener('fetch', (event) => {
@@ -29,3 +8,27 @@ self.addEventListener('fetch', (event) => {
     })
   )
 })
+
+
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('calc_schlumberger').then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./img/logo-purple.png",
+ 
+        "./img/demo/favicon.ico",
+        "./img/demo/apple-touch-icon-57x57.png",
+        "./img/demo/apple-touch-icon-72x72.png",
+        "./img/demo/apple-touch-icon-76x76.png",       
+        "./css/bootstrap.min.css",
+        "./js/object.js",
+        "./js/formula.js",
+        "./js/form.js",
+        ].map(url => new Request(url, {credentials: 'same-origin'})))
+    })
+    )
+ })
+ 
